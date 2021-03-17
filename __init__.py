df314edf2aee4bd8aaf96e492a111d020f21b673
Xand Meaden <xand@xand.uk>
2021-03-17 17:08:31 +0000

    auth_api

--- __init__.py
+++ __init__.py
@@ -58,8 +58,8 @@ def get_ldap_user(username):
     if len(result) != 2:
         return {}
 
-    sys.stderr.write(result[0])
-    sys.stderr.write(result[1])
+    sys.stderr.write(type(result[0]))
+    sys.stderr.write(type(result[1]))
     return result[1][0][1]
 
 '''
