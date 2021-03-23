51c25d89e6efdfb24bd6ba26ebdcf24af67a482c
Xand Meaden <xand@xand.uk>
2021-03-23 18:35:24 +0000

    db schema update

--- db_schema.sql
+++ db_schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE `vpn_certs` (
   `public_cert` text COLLATE ascii_general_ci NOT NULL,
   `status` enum('active','revoked') COLLATE ascii_general_ci NOT NULL DEFAULT 'active',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `vpn_certs_cn_unique` (`cn`),
+  UNIQUE KEY `vpn_certs_uuid_unique` (`uuid`),
   KEY `vpn_device_user_id_status_index` (`user_id`,`status`),
   CONSTRAINT `vpn_device_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
