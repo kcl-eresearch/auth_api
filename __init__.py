39756a2be5f3f94b3c522b5473515441e1c14437
Xand Meaden <xand@xand.uk>
2021-04-29 18:28:39 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -201,7 +201,7 @@ def revoke_vpn_key(username, key_name):
 
     try:
         for certificate in result:
-            cursor.execute("UPDATE vpn_keys SET status = 'revoked' WHERE id = %s", (certificate["id"]))
+            cursor.execute("UPDATE vpn_keys SET status = 'revoked' WHERE id = %s", (certificate["id"],))
             cert_data = x509.load_pem_x509_certificate(certificate["public_cert"].encode("utf8"), default_backend())
             serial_number = str(cert_data.serial_number)
             token = subprocess.check_output([config["ca"]["exe"], "ca", "token", "--provisioner", config["ca"]["provisioner"], "--password-file", "/etc/auth_api/ca_password.txt", "--ca-url", config["ca"]["url"], "--root", config["ca"]["root_crt"], "--revoke", serial_number]).strip()
