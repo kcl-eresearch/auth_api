2536347dd181ce1f1ab2e0d693777adc578725e4
Xand Meaden <xand@xand.uk>
2021-04-30 17:33:13 +0100

    debug

--- __init__.py
+++ __init__.py
@@ -345,6 +345,7 @@ def send_email(username, service):
         smtp.sendmail(smtp_config["from_addr"], result[0]["email"], )
         smtp.quit()
     except Exception as e:
+        raise e
         sys.stderr.write(f"Failed sending mail message: {e}\n")
         return False
 
