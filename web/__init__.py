c40ae688f18cd0ebf50f162e74703aebc82e74fb
Xand Meaden <xand@xand.uk>
2021-05-13 14:46:40 +0100

    handle MFA rejections better

--- web/__init__.py
+++ web/__init__.py
@@ -243,7 +243,7 @@ def get_mfa_requests(username, service="all"):
     requests = []
     try:
         cursor = cnx.cursor(dictionary=True)
-        cursor.execute("SELECT created_at, updated_at, expires_at, service, remote_ip, status FROM mfa_requests WHERE user_id = %s", (user_id,))
+        cursor.execute("SELECT created_at, updated_at, expires_at, service, remote_ip, status FROM mfa_requests WHERE user_id = %s AND (expires_at IS NULL OR expires_at > NOW())", (user_id,))
         result = cursor.fetchall()
     except Exception as e:
         sys.stderr.write(f"Failed getting {service} MFA requests for {username}: {e}")
@@ -634,7 +634,7 @@ def api_set_mfa_request(username):
 
     try:
         cursor = cnx.cursor()
-        cursor.execute("UPDATE mfa_requests SET status = %s, updated_at = NOW(), expires_at = %s WHERE user_id = %s AND service = %s AND remote_ip = %s", (mfa_request["status"], datetime.datetime.now() + datetime.timedelta(days=config["main"]["mfa_valid_days"]), user_id, mfa_request["service"], mfa_request["ip_address"]))
+        cursor.execute("UPDATE mfa_requests SET status = %s, updated_at = NOW(), expires_at = %s WHERE user_id = %s AND service = %s AND remote_ip = %s", (mfa_request["status"], datetime.datetime.now() + datetime.timedelta(minutes=config["main"]["mfa_timeout"][mfa_request["status"]]), user_id, mfa_request["service"], mfa_request["ip_address"]))
         cnx.commit()
     except Exception as e:
         sys.stderr.write(f"Failed updating MFA request for {username}: {e}\n")
