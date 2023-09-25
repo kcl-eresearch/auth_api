6657bd28b2cf761ede3a32125614a3066ee6d549
Xand Meaden <xand.meaden@kcl.ac.uk>
2023-09-25 09:38:52 +0100

    chore: update schema

--- config/db_schema.sql
+++ config/db_schema.sql
@@ -48,7 +48,7 @@ CREATE TABLE `ssh_keys` (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `pub_key` varchar(1024) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   `allowed_ips` json DEFAULT NULL,
-  `access_type` enum('sftp','rsync','any') COLLATE utf8mb4_unicode_ci DEFAULT 'any',
+  `access_type` enum('sftp','rsync','any','sftp_ro','rsync_ro') COLLATE utf8mb4_unicode_ci DEFAULT 'any',
   PRIMARY KEY (`id`),
   UNIQUE KEY `ssh_keys_pub_key_unique` (`user_id`,`pub_key`),
   CONSTRAINT `ssh_keys_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
