df071dc54c515b9f886eebcbe66211ab5c8ac932
Xand Meaden <xand.meaden@kcl.ac.uk>
2024-02-01 17:02:15 +0000

    fix: migrations table creation

--- auth_api/__init__.py
+++ auth_api/__init__.py
@@ -62,7 +62,7 @@ def migrate_database(mysql, migrations_path):
 
         with db.cursor() as cursor:
             cursor.execute(
-                "CREATE TABLE IF NOT EXISTS migrations(migration VARCHAR(255), id bigint unsigned NOT NULL AUTO_INCREMENT, PRIMARY_KEY (id))"
+                "CREATE TABLE IF NOT EXISTS migrations(migration VARCHAR(255), id bigint unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))"
             )
 
         with db.cursor() as cursor:
