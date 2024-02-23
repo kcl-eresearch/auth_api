c6555043398d9335e6430cc9e230a44c20766689
Xand Meaden <xand.meaden@kcl.ac.uk>
2024-02-23 09:29:38 +0000

    feat: support extra rsync options for ssh keys
    write-only, directory-locked

--- db/migrations/002_initial ssh_keys.sql
+++ db/migrations/002_initial ssh_keys.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS `ssh_keys` (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `pub_key` varchar(1024) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   `allowed_ips` json DEFAULT NULL,
-  `access_type` enum('sftp','rsync','any','sftp_ro','rsync_ro') COLLATE utf8mb4_unicode_ci DEFAULT 'any',
+  `access_type` enum('sftp','rsync','any','sftp_ro','rsync_ro','rsync_wo') COLLATE utf8mb4_unicode_ci DEFAULT 'any',
+  `extra_options` json DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ssh_keys_pub_key_unique` (`user_id`,`pub_key`),
   CONSTRAINT `ssh_keys_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
